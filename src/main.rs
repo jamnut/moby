5553427183d0864c5b992a64274cd03fd967e940
Steve Weeks <nbxsteve@gmail.com>
2023-09-30 20:20:07 -0400

    fft sizes and slide sizes

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
 #![deny(clippy::all)]
 #![forbid(unsafe_code)]
 
-mod signal;
 mod draw;
+mod signal;
 
 use std::fs::File;
 use std::io::{self, Read};
-
+use core::ops::Range;
 
 use byteorder::{BigEndian, ByteOrder, ReadBytesExt};
+use draw::draw_upper_signal;
 use pixels::{Error, Pixels, SurfaceTexture};
 use winit::dpi::LogicalSize;
 use winit::event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent};
 use winit::event_loop::EventLoop;
 use winit::window::WindowBuilder;
+
+use crate::signal::*;
+use crate::draw::*;
 
 const WIDTH: u32 = 1200;
 const HEIGHT: u32 = 800;
@@ -38,7 +42,7 @@ fn main() -> Result<(), Error> {
         Pixels::new(WIDTH, HEIGHT, surface_texture)?
     };
 
-    let mut m = Model::new(28);
+    let mut m = Model::new(74);
 
     event_loop.run(move |event, _, control_flow| {
         control_flow.set_wait();
@@ -55,7 +59,7 @@ fn main() -> Result<(), Error> {
             }
 
             Event::RedrawRequested(_) => {
-                draw::draw(pixels.frame_mut(), &m);
+                draw::draw(pixels.frame_mut(), &m).unwrap();
                 if let Err(err) = pixels.render() {
                     println!("Error: {err:?}");
                     control_flow.set_exit();
@@ -64,6 +68,15 @@ fn main() -> Result<(), Error> {
 
             Event::MainEventsCleared => {
                 // window.request_redraw();
+            }
+
+            Event::WindowEvent { 
+                event:
+                    WindowEvent::ModifiersChanged(mods),
+                ..
+            } => {
+                m.modifiers = mods;
+                println!("{:?}", mods);
             }
 
             Event::WindowEvent {
@@ -79,20 +92,27 @@ fn main() -> Result<(), Error> {
                     },
                 ..
             } => {
+                use VirtualKeyCode::*;
                 match key {
-                    VirtualKeyCode::Up => m.next(),
-                    VirtualKeyCode::Down => m.prev(),
-                    VirtualKeyCode::Left => m.window_start = m.window_start.saturating_sub(100),
-                    VirtualKeyCode::Right => m.window_start = 3800.min(m.window_start + 100),
-                    VirtualKeyCode::Q => control_flow.set_exit(),
+                    Up => m.next_file(),
+                    Down => m.prev_file(),
+                    Left => m.prev_window(),
+                    Right => m.next_window(),
+                    F => m.fft_size.rotate_left(1),
+                    N => m.max64.rotate_left(1),
+                    P => play_aiff(&m),
+                    Q => control_flow.set_exit(),
+                    S => m.slide.rotate_left(1),
+                    U => m.upper.rotate_left(1),
                     _ => println!("Key pressed {key:?}"),
-                };
+                }
                 window.request_redraw();
             }
             _ => (),
         }
     })
 }
+
 
 // Takes a integer and returns the file name
 fn aiff_name(file_number: usize) -> (String, String) {
@@ -105,58 +125,118 @@ fn aiff_name(file_number: usize) -> (String, String) {
 
 fn aiff_samples(full_name: &str) -> Result<Vec<i16>, io::Error> {
     let mut file = File::open(full_name)?;
-    
+
     // Read and skip the AIFF header
     let mut header_buf = [0u8; 54];
     file.read_exact(&mut header_buf)?;
-    
+
     // Parse the AIFF header to determine audio data size
-    let num_samples = BigEndian::read_u32(&header_buf[22..26]);
-    
-    // Calculate the number of samples to read (16-bit samples)
-    let num_samples_to_read = std::cmp::min(num_samples, 4000);
-    
+    let num_samples = BigEndian::read_u32(&header_buf[22..26]).min(4000);
+
     // Read and convert the audio samples
-    let mut samples = Vec::with_capacity(num_samples_to_read as usize);
-    for _ in 0..num_samples_to_read {
+    let mut samples = Vec::with_capacity(num_samples as usize);
+    for _ in 0..num_samples {
         let sample = file.read_i16::<BigEndian>()?;
         samples.push(sample);
     }
-    
+
     Ok(samples)
 }
 
-pub struct Model {
+pub struct Model<'a> {
     aiff_number: usize,
     aiff_name: String,
-    aiff_path: String,
+    full_name: String,
     aiff_data: Vec<i16>,
-    window_start: usize,
+    fft_size: Vec<usize>,
+    start: usize,
+    window: usize,
+    slide: Vec<usize>,
+    modifiers: winit::event::ModifiersState,
+    max64: Vec<(fn(&Model, Range<usize>) -> f64, &'a str)>,
+    upper: Vec<fn(draw::MyDrawingArea, &Model) -> Result<(), Box<dyn std::error::Error>>>,
+
 }
 
-impl Model {
+impl Model<'_> {
     fn new(aiff_number: usize) -> Self {
         let (aiff_name, aiff_path) = aiff_name(aiff_number);
         let aiff_data = aiff_samples(&aiff_path).unwrap();
 
-        Self { aiff_number, aiff_name, aiff_path, aiff_data, window_start: 0 }
+        Self {
+            aiff_number,
+            aiff_name,
+            full_name: aiff_path,
+            aiff_data,
+            fft_size: vec![1024,512,256,128],
+            start: 2000,
+            window: 200,
+            slide: vec![10, 50, 100, 200],
+            modifiers: winit::event::ModifiersState::default(),
+            max64: vec![(max_window, "nwindow"), (max_none, "nnone"), (max_all, "nall")],
+            upper: vec![draw_upper_fft, draw_upper_signal],
+        }
     }
 
     fn set_aiff(&mut self, aiff_number: usize) {
-        if aiff_number > 30_000 { return; }
-        if aiff_number < 1 { return; }
+        if aiff_number > 30_000 {
+            return;
+        }
+        if aiff_number < 1 {
+            return;
+        }
 
         self.aiff_number = aiff_number;
-        (self.aiff_name, self.aiff_path) = aiff_name(self.aiff_number);
-        self.aiff_data = aiff_samples(&self.aiff_path).unwrap();
+        (self.aiff_name, self.full_name) = aiff_name(self.aiff_number);
+        self.aiff_data = aiff_samples(&self.full_name).unwrap();
     }
 
-    fn next(&mut self) {
+    fn next_file(&mut self) {
         self.set_aiff(self.aiff_number + 1);
     }
 
-    fn prev(&mut self) {
+    fn prev_file(&mut self) {
         self.set_aiff(self.aiff_number - 1);
     }
 
+    fn range(&self) -> std::ops::Range<usize> {
+        self.start..self.start + self.window
+    }
+
+    fn next_window(&mut self) {
+        use winit::event::*;
+        self.start = (4000 - self.window).min(self.start +
+            match self.modifiers {
+                ModifiersState::SHIFT => 10,
+                _ => 100.max(self.slide[0]),
+            });
+    }
+    
+    fn prev_window(&mut self) {
+        use winit::event::*;
+        self.start = self.start.saturating_sub(
+            match self.modifiers {
+                ModifiersState::SHIFT => 10,
+                _ => 100.max(self.slide[0]),
+            });
+        }
+
+        
 }
+
+
+fn play_aiff(m: &Model) {
+    use rodio::{OutputStream, Sink};
+    use rodio::buffer::SamplesBuffer;
+
+    let max = 0.7 * m.max64[0].0(m, m.range()) as f32;
+    let signal: Vec<f32> = m.aiff_data.iter().map(|x| *x as f32 / max).collect();
+  
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
+
+    let source = SamplesBuffer::new(1, 2000, signal);
+    sink.append(source);
+    sink.sleep_until_end();
+}
+
--- src/signal.rs
+++ src/signal.rs
@@ -2,89 +2,136 @@
 #![forbid(unsafe_code)]
 
 use std::f32::consts::PI;
+use std::ops::Range;
 
 use num::cast::NumCast;
-use num::traits::{Num, Float};
+use num::traits::{Float, Num};
 
-fn to_f64<T>(s: T) -> f64
-where   
-T: Num + NumCast + Copy,
-{
-    NumCast::from(s).unwrap()
-}
+use crate::Model;
 
-fn to_float<T, T2>(s: T) -> T2
-where
-T: Num + NumCast + Copy,
-T2: Float + NumCast + Copy,
-{
-    NumCast::from(s).unwrap()
-}
-
-pub fn max<T>(data: &[T]) -> T
-where
-T: Float + NumCast + Copy,
-{
-    data.iter().fold(T::neg_infinity(), |a, b| a.max(*b))
-}
-
-pub fn normalize<T, T2>(data: &[T]) -> Vec<T2>
-where
-T: Num + NumCast + Copy,
-T2: Float + NumCast + Copy,
-{
-    let data: Vec<T2> = data.iter().map(|s| to_float(*s)).collect();
-
-    let amax = max(&data);
-
-    data.iter().map(|x| *x / amax).collect()
-}    
-
-pub fn series<T>(aiff: &[T]) -> Vec<(usize, f64)> 
-where
-T: Num + NumCast + Copy,
-{
-    aiff
-    .iter()
-    .enumerate()
-    .map(|(i, s)| (i, to_f64(*s)))
-    .collect()
-}
 
 pub fn _hann(samples: &mut [f32]) {
     let len = samples.len();
     for (i, item) in samples.iter_mut().enumerate().take(len) {
         *item *= 0.5 * (1.0 - (2.0 * PI * i as f32 / (len - 1) as f32).cos());
     }
-}    
+}
 
-pub fn amplitudes(window: usize, aiff: &[f32]) -> Vec<f32> {
-
-  let mut samples: [f32; 1024] = [0.0; 1024];  
-  samples[0..200].copy_from_slice(&aiff[window..window+200]);
-//   hann(&mut samples);
-
-  let spectrum = microfft::real::rfft_1024(&mut samples);
-  spectrum[0].im = 0.0;
-
-  spectrum.iter().map(|c| c.norm()).collect()
-}  
-
-pub fn _median(numbers: &mut Vec<i32>) -> i32 {
+pub fn _median(numbers: &mut [f32]) -> f32 {
     let length = numbers.len();
 
     if length == 0 {
-        return 0;
+        return 0.0;
     }
 
-    numbers.sort_unstable();
+    numbers.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap());
 
     if length % 2 == 0 {
         let left = numbers[length / 2];
         let right = numbers[length / 2 + 1];
 
-        (left + right) / 2
+        (left + right) / 2.0
     } else {
         numbers[length / 2]
     }
 }
+
+fn _to_float<T, T2>(s: T) -> T2
+where
+    T: Num + NumCast + Copy,
+    T2: Float + NumCast + Copy,
+{
+    NumCast::from(s).unwrap()
+}
+
+pub fn maximum<T>(data: &[T]) -> T
+where
+    T: Float + NumCast + Copy,
+{
+    data.iter().fold(T::neg_infinity(), |a, b| a.max(*b))
+}
+
+// TODO: This really should be abs max, not plain max
+pub fn max_window(m: &Model, range: Range<usize>) -> f64 {
+    *(m.aiff_data[range].iter().max().unwrap()) as f64
+}
+
+pub fn max_all(m: &Model, _range: Range<usize>) -> f64 {
+    *(m.aiff_data.iter().max().unwrap()) as f64
+}
+
+pub fn max_none(_m: &Model, _range: Range<usize>) -> f64 {
+    i16::MAX as f64
+}
+
+pub fn fft(m: &Model, range: Range<usize>) -> (Vec<f32>, f32) {
+
+    let dmax: f32 = m.max64[0].0(m, range.clone()) as f32;
+    let data: Vec<f32> = m.aiff_data.iter().map(|x| *x as f32 / dmax).collect();
+
+
+    // hann(&mut samples);
+
+    let fft: Vec<f32> = match m.fft_size[0] {
+        1024 => {
+            let mut samples: [f32; 1024] = [0.0; 1024];
+            samples[0..200].copy_from_slice(&data[range.clone()]);
+            let fft = microfft::real::rfft_1024(&mut samples);
+            fft[0].im = 0.0;
+            fft.iter().map(|c| c.norm()).collect()
+        },
+        512 =>  {
+            let mut samples: [f32; 512] = [0.0; 512];
+            samples[0..200].copy_from_slice(&data[range.clone()]);
+            let fft = microfft::real::rfft_512(&mut samples);
+            fft[0].im = 0.0;
+            fft.iter().map(|c| c.norm()).collect()
+        },
+        256 =>  {
+            let mut samples: [f32; 256] = [0.0; 256];
+            samples[0..200].copy_from_slice(&data[range.clone()]);
+            let fft = microfft::real::rfft_256(&mut samples);
+            fft[0].im = 0.0;
+            fft.iter().map(|c| c.norm()).collect()
+        },
+        128 =>  {
+            let end = range.end.min(range.start + 128);
+            let mut samples: [f32; 128] = [0.0; 128];
+            samples[0..128].copy_from_slice(&data[range.start..end]);
+            let fft = microfft::real::rfft_128(&mut samples);
+            fft[0].im = 0.0;
+            fft.iter().map(|c| c.norm()).collect()
+        },
+        _ => panic!("fft size not supported"),
+    };
+
+    let fft: Vec<f32> = fft.iter().map(|x| x * x).collect();
+
+    let bin_size = 2000.0 / m.fft_size[0] as f32;
+    let bin_range = (25.0 / bin_size) as usize .. (400.0 / bin_size + 1.0) as usize;
+  
+    let fmax = maximum(&fft[bin_range]);
+    let fft = fft.iter().map(|x| *x / fmax).collect();
+
+    (fft, fmax)
+}
+
+pub fn spectrogram(m: &Model) -> (Vec<Vec<f32>>, Vec<f32>, f32)  {
+
+    let mut spec: Vec<Vec<f32>> = Vec::new();
+    let mut fmax: Vec<f32> = Vec::new();
+    let mut smax: f32 = 0.0;
+
+    for i in (0..4000 - m.window).step_by(m.slide[0]) {
+        let range = i..i + m.window;
+        let (fft, max) = fft(m, range);
+    
+        spec.push(fft);
+        fmax.push(max);
+        smax = smax.max(max);
+    }
+
+    (spec, fmax, smax)
+
+}
+
